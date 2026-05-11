90fdcd66dbcbff6d61dc212e20100184ff12b0f1
AKIANA Arsene <akianaarsenegloire@gmail.com>
2026-05-12 00:56:21 +0100

    version 1 de ex1

---
+++ ex1/main.cpp
@@ -0,0 +1,29 @@
+// <------------- INTRODUCTION --------------->
+#include <iostream>
+
+/*
+    std::cout : sortie (affichage)standard  (en memoire tampon(buffer))
+    std::cerr : sortie d'erreur standard (affichage direct)
+    std::clog : journalisation (en memoire tampon)
+
+    std::endl : retour à la ligne (\n) + flush (vide la mémoire tampon et affiche tout ce qui s'y trouve)
+
+    \n  : retour à ligne
+    \t : tabulation
+    \\ : afficher un antislash
+*/
+
+
+int main()
+{
+       std::cout  <<  "Hello World !" << "How are you ?" << " i'm test for gitHub version 1 " << std::endl;
+       std::cout << "Bye !!! ";
+       return 0;
+}
+
+
+
+
+
+
+
